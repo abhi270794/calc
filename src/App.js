601982506b979707dcda366d995fedcc04c7e83e
Abhishek Soni <asoni2@icims.com>
2024-03-18 12:22:20 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [result,setResult] = useState("");
+const clickHandler=(event)=>{
+  setResult(result.concat(event.target.value))
+}
+const clear=()=>{
+  setResult("");
+}
+const calculate=()=>{
+  setResult(eval(result).toString());
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='calc'>
+      <input type="text" placeholder ="0" id="answer" value={result}/>
+      <div className='button'> 
+      <input type="button" value="9" onClick={clickHandler}/>
+      <input type="button" value="8" onClick={clickHandler}/>
+      <input type="button" value="7" onClick={clickHandler}/>
+      <input type="button" value="6" onClick={clickHandler}/>
+      <input type="button" value="5" onClick={clickHandler}/>
+      <input type="button" value="4" onClick={clickHandler}/>
+      <input type="button" value="3" onClick={clickHandler}/>
+      <input type="button" value="2" onClick={clickHandler}/>
+      <input type="button" value="1" onClick={clickHandler}/>
+      <input type="button" value="0" onClick={clickHandler}/>
+      <input type="button" value="*" onClick={clickHandler}/>
+      <input type="button" value="-" onClick={clickHandler}/>
+      <input type="button" value="+" onClick={clickHandler}/>
+      <input type="button" value="%" onClick={clickHandler}/>
+      <input type="button" value="=" onClick={calculate}/>
+      <input type="button" value="Clear" style={{width: "96px"}}onClick={clear}/>
+      </div>
+      </div>
     </div>
   );
 }
